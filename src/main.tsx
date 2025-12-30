58c7cf472454a21e5fd4c562a13bae4022e1df57
Tom <keith.chen.dev@gmail.com>
2025-12-29 20:23:11 -0800

    potential fix for vercel

--- src/main.tsx
+++ src/main.tsx
@@ -13,11 +13,11 @@ createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/:id" element={<Post />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
+        <Route path="/:id" element={<Post />} />
       </Routes>
     </BrowserRouter>
-  </StrictMode>
+  </StrictMode>,
 );
