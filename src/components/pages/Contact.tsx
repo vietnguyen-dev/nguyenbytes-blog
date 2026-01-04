b8f4ad5b42787c06f95737591077780e898154fc
Tom <keith.chen.dev@gmail.com>
2026-01-03 23:08:43 -0800

    xool stauff

--- src/components/pages/Contact.tsx
+++ src/components/pages/Contact.tsx
@@ -5,10 +5,12 @@ import Popular from "../UI/Popular";
 const Contact = () => {
   return (
     <Page>
-      <h2 className="text-3xl font-bold mt-6 mb-3">Contact</h2>
-      <p className="mb-3">Contact me at the below options!</p>
-      <ContactForm showHeading={false} />
-      <Popular />
+      <div className="lg:w-3/5 lg:mx-auto lg:px-8">
+        <h2 className="font-bold mb-4 text-gray-400 font-medium tracking-widest px-2 mt-6">CONTACT</h2>
+        <p className="mb-3">Contact me at the below options!</p>
+        <ContactForm showHeading={false} />
+        <Popular />
+      </div>
     </Page>
   );
 };
